5fd1a91e12e957c837617a9473e73051d27adf57
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-22 20:20:43 +0200

    add user instructions

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import {example} from './js/example'
 import './styles/index.scss'
 
 example();
+
+// Here an example to import an image in the HTML file :
+
+// import myImage from './images/my-image.svg'
+
+// const myImageElement = document.createElement('img')
+// myImageElement.src = myImage
+
+// const myImageDiv = document.querySelector('#box')
+
+// myImageDiv.append(box)
